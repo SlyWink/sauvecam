58e741f79ebc9389b99f61acbc4b27ad1f84c5c8
SlyWink <24624623+SlyWink@users.noreply.github.com>
2020-06-01 15:21:40 +0200

    Download config from Github

--- chargeconf.py
+++ chargeconf.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#import pexpect
-import glob
 import os
 import time
-import sys
 import stat
-# Version modifiée comportant codes 204 et 501 si collection existe (ano 4shared)
-sys.path.append('/home/pi/easywebdav-master_SL')
-import easywebdav
-import subprocess
-import base64
+import requests
+
 
 DEBUG = 1
+ESSAIS = 5
+PAUSE = 30
 HOSTNAME = os.uname()[1].upper()
-DISTANT = '/' + 'Config/'
+DISTANT = 'https://raw.githubusercontent.com/SlyWink/sauvecam/master/'
 LOCAL = '/tmp/picam/'
-
-execfile('/home/pi/cloudconf.py')
-
-def debug(msg):
-  if DEBUG:
-    print time.strftime("%d/%m/%y-%H:%M:%S",time.localtime()) + " - " + msg
-
-
-time.sleep(5)
+MSG_OK = ' -> OK'
 
 SCRIPT_D = DISTANT + 'sauvecam.py'
 SCRIPT_L = LOCAL + 'sauvecam.py'
@@ -34,26 +22,42 @@ CONFIG_L = LOCAL + 'motion.conf'
 MASQUE_D = DISTANT + HOSTNAME + '_masque.pgm'
 MASQUE_L = LOCAL + 'masque.pgm'
 
-while True:
+
+def debug(msg):
+  if DEBUG:
+    print time.strftime("%d/%m/%y-%H:%M:%S",time.localtime()) + " - " + msg
+
+
+def wget(inName,outName=''):
+  r = requests.get(inName)
+  if r.status_code == requests.codes.ok:
+    open(outName , 'wb').write(r.content)
+    return True
+  return False
+
+
+essai = 0
+
+while essai < ESSAIS:
+  essai += 1
 
   try:
-    # Paramétrage connexion webdav
-    debug("Paramétrage connexion " + CLOUD)
-    wd = easywebdav.connect(host=CLOUD,username=USERNAME,password=base64.decodestring(PASSWORD),protocol=PROTO)
 
     debug("Téléchargement " + SCRIPT_D + " en " + SCRIPT_L)
-    wd.download(SCRIPT_D,SCRIPT_L)
-    os.chmod(SCRIPT_L,stat.S_IRWXU|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
+    if wget(SCRIPT_D,SCRIPT_L):
+      debug(MSG_OK)
+      os.chmod(SCRIPT_L,stat.S_IRWXU|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
 
     debug("Téléchargement " + CONFIG_D + " en " + CONFIG_L)
-    wd.download(CONFIG_D,CONFIG_L)
+    if wget(CONFIG_D,CONFIG_L):
+      debug(MSG_OK)
 
-    if wd.exists(MASQUE_D):
-      debug("Téléchargement " + MASQUE_D + " en " + MASQUE_L)
-      wd.download(MASQUE_D,MASQUE_L)
+    debug("Téléchargement " + MASQUE_D + " en " + MASQUE_L)
+    if wget(MASQUE_D,MASQUE_L):
+      debug(MSG_OK)
 
     break
 
-  except:
-    debug("Anomalie téléchargement, nouvel essai dans 30s")
-    time.sleep(30)
+  except requests.exceptions.RequestException:
+    debug("Anomalie téléchargement, nouvel essai dans %ds" % (PAUSE))
+    time.sleep(PAUSE)
