1c4815fadcedb92271cf5b9208afe85f34fbb110
SlyWink <24624623+SlyWink@users.noreply.github.com>
2020-06-06 22:29:21 +0200

    Program rewrited to better handle webdav exceptions

--- sauvecam.py
+++ sauvecam.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-#import pexpect
 import glob
 import os
 import time
@@ -16,38 +15,32 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
 DEBUG = 1
-DISTANT = '/' + os.uname()[1].upper()
+HOSTNAME = os.uname()[1].upper()
 CAPTURES = '/tmp/picam'
 MAXFICH = 1000
 
 execfile("/home/pi/cloudconf.py")
 
 CONSERVER = 20 * 1024 * 1024
-
-TRUE = 1
-FALSE = 0
-
-courant = ''
-attente = 1
-derdate = 0
-cptphot = 0
+UPTIME_SEC = 5 * 60
+TIMEOUT_SEC = 20
 
 def debug(msg):
   if DEBUG:
-    print time.strftime("%d/%m/%y-%H:%M:%S",time.localtime()) + " - " + msg
+    print time.strftime("%d/%m/%y-%H:%M:%S", time.localtime()) + " - " + msg
 
 def uptime():
   with open('/proc/uptime', 'r') as f:
-      seconds = float(f.readline().split()[0])
-      return int(seconds)
+    seconds = float(f.readline().split()[0])
+    return int(seconds)
 
 def mailInfo():
   try:
     msg = MIMEMultipart()
     msg['From'] = FROM_ADDR
     msg['To'] = TO_ADDR
-    subject = "Reboot" if (uptime() < 5*60) else "Anomalie"
-    msg['Subject'] = "Pi %s : %s" % (os.uname()[1],subject)
+    subject = "Reboot" if (uptime() < UPTIME_SEC) else "Anomalie"
+    msg['Subject'] = "[%s] -> %s" % (HOSTNAME, subject)
     body = "Voir ce qui se passe"
     msg.attach(MIMEText(body, 'plain'))
     server = smtplib.SMTP_SSL(SMTP_SRV)
@@ -60,90 +53,117 @@ def mailInfo():
 
 def dispo(path):
   stat = os.statvfs(path)
-  return stat.f_bavail * stat.f_frsize 
+  return stat.f_bavail * stat.f_frsize
 
-def handler(signum,frame):
-  print "** TIME OUT **"
-  raise easywebdav.WebdavException("Time out")
+
+class MyWebdav:
+
+  @staticmethod
+  def _handler(signum, frame):
+    print "** TIME OUT **"
+    raise easywebdav.WebdavException("Time out")
+
+  def __init__(self, wd):
+    self._wd = wd
+    self._fileCount = 0
+    self._previousCollection = None
+    self.lastTime = 0
+    signal.signal(signal.SIGALRM, self.__class__._handler)
+
+  def _path2Collection(self, filePath):
+    fileName = os.path.basename(filePath)
+    # Récupère la date du fichier et la mémorise si elle est plus récente
+    # %v-%Y%m%d%H%M%S-%q
+    fileTimestamp = fileName.split('-')[1]
+    fileTime = time.mktime(time.strptime(fileTimestamp, "%Y%m%d%H%M%S"))
+    if self.lastTime == 0 or self.lastTime < fileTime:
+      self.lastTime = fileTime
+    # Extrait le nom du répertoire destinataire
+    fileDate = fileTimestamp[0:8]
+    fileHour = int(fileTimestamp[8:10])
+    collectionHour = "00-05"
+    if fileHour >= 6:
+      # Captures h et h+1 (h paire) dans le même répertoire
+      evenHour = fileHour - fileHour % 2
+      collectionHour = "%02d-%02d" % (evenHour, evenHour+1)
+    return [fileDate, collectionHour, fileName]
+
+  def upload(self, filePath):
+    collectionItems = self._path2Collection(filePath)
+    fileName = collectionItems.pop()
+    collectionPath = "/" + HOSTNAME + "/" + collectionItems[0]
+    signal.alarm(TIMEOUT_SEC)
+    try:
+      if collectionItems != self._previousCollection:
+        self._fileCount = 0
+        debug("Lecture contenu de la collection " + collectionPath)
+        collectionRead = len(self._wd.ls(collectionPath))
+        debug(" -> %d" % collectionRead)
+        if collectionRead == 0:
+          debug("Création de la collection " + collectionPath)
+          self._wd.mkdir(collectionPath)
+        else:
+          index = 0
+          while True:
+            collectionSubPath = "%s/%s.%1d" % (collectionPath, collectionItems[1],index)
+            debug("Lecture nombre de fichiers de la collection " + collectionSubPath)
+            count = len(self._wd.ls(collectionSubPath))
+            debug(" -> %d" % count)
+            if count == 0:
+              break
+            else:
+              self._fileCount += count -1
+              index += 1
+      collectionPath += "/%s.%1d" % (collectionItems[1], self._fileCount / MAXFICH)
+      if self._fileCount % MAXFICH == 0:
+        debug("Création de la collection " + collectionPath)
+        self._wd.mkdir(collectionPath)
+      remotePath = collectionPath + "/" + fileName
+      debug("Transfert %s => %s (%d)" % (filePath, remotePath,self._fileCount))
+      startTime = time.time()
+      wd._wd.upload(filePath, remotePath)
+      debug(" -> OK en %.2f s" % (time.time() - startTime))
+      self._previousCollection = collectionItems
+      self._fileCount += 1
+      return True
+    except easywebdav.WebdavException, texterr:
+      debug("Anomalie WebDAV\n{}".format(texterr))
+      return False
+    finally:
+      signal.alarm(0)
+
+
 
 # Courriel d'avertissement
 mailInfo()
 
 # Paramétrage connexion webdav
 debug("Paramétrage connexion " + CLOUD)
-wd = easywebdav.connect(host=CLOUD,username=USERNAME,password=base64.decodestring(PASSWORD),protocol=PROTO)
+wd = MyWebdav(easywebdav.connect(host=CLOUD, username=USERNAME, password=base64.decodestring(PASSWORD), protocol=PROTO))
 
-signal.signal(signal.SIGALRM,handler)
-while TRUE:
+attente = True
+while True:
 
-  try:
-    photos = glob.glob(CAPTURES + '/[0-9]*.jpg')
-    if photos:
-      debug("Présence de photos")
-      attente = 1
-      photos.sort()
-      for path in photos:
-        # Extrait le nom du fichier
-        photo = os.path.basename(path)
-        # Récupère la date du fichier et la mémorise si elle est plus récente
-        # %v-%Y%m%d%H%M%S-%q
-        datephot = photo.split('-')[1]
-        datefich = time.mktime(time.strptime(datephot,"%Y%m%d%H%M%S"))
-        if derdate == 0 or derdate < datefich:
-          derdate = datefich
-        # Vérifie l'espace disque disponible
-        if dispo(CAPTURES) < CONSERVER:
-          debug("Plus de place, suppression photo " + photo)
-        else:
-          debug("Traitement " + photo)
-          # Extrait le nom du répertoire destinataire
-          repdist_j = datephot[0:8]
-          heure = int(datephot[8:10])
-          repdist_h = ""
-          if heure < 6:
-            # Captures entre 00:00 et 05:59 dans le même répertoire
-            repdist_h = "00-05"
-          else:
-            # Captures h et h+1 (h paire) dans le même répertoire
-            hpaire = heure - heure % 2
-            repdist_h = "%02d-%02d" % (hpaire,hpaire+1)
-          repdist = repdist_j + '/' + repdist_h
-          if courant[0:-2] <> repdist:
-            cptphot = 0
-          repdist += ".%1d" % (cptphot / MAXFICH)
-          # 20 secondes maxi pour faire le transfert
-          signal.alarm(20)
-          # On vérifie si le répertoire a été traité récemment
-          if repdist <> courant:
-            # Vérifie l'existence du répertoire jour
-            if not wd.exists(DISTANT + '/' + repdist_j):
-              # Création du répertoire jour si inexistant
-              debug("Création de la collection " + repdist_j)
-              wd.mkdir(DISTANT + '/' + repdist_j)
-            # Vérifie l'existence du répertoire jour+heure
-            if not wd.exists(DISTANT + '/' + repdist):
-              # Création du répertoire jour+heure si inexistant
-              debug("Création de la collection " + repdist)
-              wd.mkdir(DISTANT + '/' + repdist)
-            courant = repdist
-          debug("Transfert " + path + " => " + DISTANT + "/" + repdist)
-          heure = time.time()
-          wd.upload(path,DISTANT + '/' + repdist + '/' + photo)
-          debug(" -> OK en %.2f s" % (time.time()-heure))
-          cptphot += 1
+  photos = glob.glob(CAPTURES + '/[0-9]*.jpg')
+  if photos:
+    debug("Présence de photos")
+    attente = True
+    photos.sort()
+    for path in photos:
+      # Vérifie l'espace disque disponible
+      if dispo(CAPTURES) < CONSERVER:
+        debug("Plus de place, suppression photo " + path)
         os.remove(path)
-  except easywebdav.WebdavException,texterr:
-    debug("Anomalie WebDAV\n{}".format(texterr))
-    courant = ''
-    pass
-  finally:
-    # Désactive l'alarme
-    signal.alarm(0)
-
+      else:
+        for tries in range(5):
+          debug("Traitement " + path)
+          if wd.upload(path):
+            os.remove(path)
+            break
   if attente:
     debug("En attente de captures")
-    attente = 0
+    attente = False
   time.sleep(5)
-  if derdate > 0 and time.time()-derdate > 3630:
+  if wd.lastTime > 0 and time.time() - wd.lastTime > 3630:
     debug("Dernière photo antérieure à 1h05, redémarrage nécessaire")
     subprocess.call("/usr/bin/sudo /sbin/reboot", shell=True)
